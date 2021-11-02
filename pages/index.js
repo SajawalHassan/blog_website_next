8275fd5d2b1f4cc31249b53f0fbbc948153ce78c
SajawalHassan <scientificninja1f12@gmail.com>
2021-11-02 21:01:29 +0500

    Finish mobile website

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ import Header from "../components/Header";
 
 export default function Home() {
   return (
-    <div className="bg-gray-300 font-mono h-[100vh]">
+    <div className="bg-gray-300 font-mono h-[250vh]">
       <Head>
         <title>Blog app</title>
         <link rel="icon" href="/favicon.ico" />
@@ -16,7 +16,15 @@ export default function Home() {
       <div className="m-5">
         <Card />
         <CardRow />
+        <Card />
+        <Card />
+        <CardRow />
+        <Card />
+        <Card />
       </div>
+      <h1 className="text-center">
+        Made by Sajawal Hassan class 5 test project.
+      </h1>
     </div>
   );
 }
